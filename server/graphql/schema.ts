96684ebd7124f3788ae185c35a75ba7da42c4e6e
Viraj <bamaking128@gmail.com>
2021-11-04 01:47:34 -0400

    :recycle: Reorganized folders and deleted server folder

--- server/graphql/schema.ts
+++
@@ -1,8 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools';
-import { typeDefs } from './TypeDefs';
-import { resolver } from './Resolver';
-
-export const schema = makeExecutableSchema({
-	typeDefs: typeDefs,
-	resolvers: resolver,
-});
